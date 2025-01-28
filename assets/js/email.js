9d1c10e0f866cde488208e0a675562afc4ea6b87
Sharjeel Ahmed <sharjlbalouch319@gmail.com>
2025-01-28 11:28:58 +0500

    Updated Portfolio

---
+++ assets/js/email.js
@@ -0,0 +1,10 @@
+function sendMail() {
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_doarzii", "template_m9c29od", parms).then(alert("Email sent successfully!"))
+}
